069b4671766d0ae018a6f61939a9bd778c52361e
Shad0w7 <ayush.nayak@gmail.com>
2020-04-24 12:17:55 -0700

    adding stuff

---
+++ helloworld/helloworld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by Ayush Nayak on 4/23/20.
+//  Copyright © 2020 Ayush Nayak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
