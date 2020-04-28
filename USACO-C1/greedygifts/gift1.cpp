69a67f0738455d872023e5f0690507fad019b291
Shad0w7 <ayush.nayak@gmail.com>
2020-04-28 00:09:03 -0700

    fixed gift1

--- USACO-C1/greedygifts/gift1.cpp
+++ USACO-C1/greedygifts/gift1.cpp
@@ -8,6 +8,7 @@ LANG: C++
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <stdio.h>
 
 using namespace std;
 
@@ -15,6 +16,11 @@ int main()
 {
     ofstream outputFile("gift1.out");
     ifstream in("gift1.in");
+    if (!in)
+    {
+        cout << "Cannot open input file.\n";
+        return 1;
+    }
     string str;
     vector<string> inputs;
 
@@ -45,25 +51,58 @@ int main()
     while (wrapCount < peopleNum)
     {
         // Check Person
+        if (position == inputs.size() - 1) {
+            break;
+        }
         string numberRAW = inputs.at(position + 1);
 
         istringstream ss(numberRAW);
         vector<int> splitRaw;
+
         do
         {
             string num;
             ss >> num;
+            if (num == "")
+            {
+                break;
+            }
             splitRaw.push_back(stoi(num));
+
         } while (ss); // Now there is a vector, splitRaw, which has [money, people]
+
+        if (splitRaw.at(1) == 0)
+        { // catch division by 0
+            wrapCount++;
+            continue;
+            
+        }
 
         int moneyToDivide = splitRaw.at(0) / splitRaw.at(1);
         int moneyToReturn = splitRaw.at(0) % splitRaw.at(1); // Calculating money to give and return
 
-        vector<string>::iterator giverIt = find(inputs.begin(), inputs.end(), inputs.at(position));
-        int indexOfGiver = distance(inputs.begin(), giverIt);
+        vector<string>::iterator giverIt = find(namesAtIndex.begin(), namesAtIndex.end(), inputs.at(position));
+        int indexOfGiver = distance(namesAtIndex.begin(), giverIt);
 
+        people[indexOfGiver] -= splitRaw.at(0);
+        people[indexOfGiver] += moneyToReturn;
+        position += 2; //now on first person!
+        for (int i = 0; i < splitRaw.at(1); i++)
+        {
+            vector<string>::iterator receiverIt = find(namesAtIndex.begin(), namesAtIndex.end(), inputs.at(position));
+            int indexOfReceiver = distance(namesAtIndex.begin(), receiverIt);
+            people[indexOfReceiver] += moneyToDivide;
+            position += 1;
+        }
+
+        wrapCount++;
+    }
+    
+    //now the money has been allocated, all that remains is to create strings and print them to the output file!!!!
+    for(int i = 0; i < peopleNum; i++) {
+        string outputVal = namesAtIndex.at(i) + " " + to_string(people[i]);
+        outputFile << outputVal << endl;
     }
 
-    outputFile << "NOTSETYET" << endl;
     return 0;
 }
