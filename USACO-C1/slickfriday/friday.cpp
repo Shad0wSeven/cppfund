0b5182bc13a0393f9359d1d5d9a48a17c3614c0d
Shad0w7 <ayush.nayak@gmail.com>
2020-04-29 11:08:08 -0700

    updated friday cpp to make it more slick

--- USACO-C1/slickfriday/friday.cpp
+++ USACO-C1/slickfriday/friday.cpp
@@ -12,28 +12,27 @@ using namespace std;
 int main()
 {
     ofstream outputFile("friday.out");
-    ifstream inputFile("friday.in");    // Setting Up Variables!!!
-    string line1;
-    inputFile >> line1;
-    int years = stoi(line1);
+    ifstream inputFile("friday.in"); // Setting Up Variables!!!
+    int years;
+    inputFile >> years;
 
     int currentYear = 1900;
     bool isLeap;
-    int dayOfTheWeek = 5; // Mon 0 Tue 1 Wed 2 Thurs 3 Fri 4 Sat 5 Sun 6     /* The Reason for the dayOfTheWeek being 5 is that the first 13'th was a Saturday! in 1900! */
-    int month = 0;        // Starts on January 
-    int dayTrack[7] = {0, 0, 0, 0, 0, 0, 0}; // Tracks Day Counter (what is outputted at the end)
+    int dayOfTheWeek = 5;                                    // Mon 0 Tue 1 Wed 2 Thurs 3 Fri 4 Sat 5 Sun 6     /* The Reason for the dayOfTheWeek being 5 is that the first 13'th was a Saturday! in 1900! */
+    int month = 0;                                           // Starts on January
+    int dayTrack[7] = {0, 0, 0, 0, 0, 0, 0};                 // Tracks Day Counter (what is outputted at the end)
     int monthMod[12] = {3, 0, 3, 2, 3, 2, 3, 3, 2, 3, 2, 3}; // Modulus for the differnet days
     int leapMod[12] = {3, 1, 3, 2, 3, 2, 3, 3, 2, 3, 2, 3};
 
     while (currentYear < 1900 + years)
     {
 
-        if ( (currentYear % 4 == 0 && currentYear %100 !=0) || currentYear % 400 == 0)
+        if ((currentYear % 4 == 0 && currentYear % 100 != 0) || currentYear % 400 == 0)
             isLeap = true;
 
         if (isLeap == 1) // Leap Year
         {
-            while (month < 12) 
+            while (month < 12)
             {
                 dayTrack[dayOfTheWeek]++;
                 dayOfTheWeek = (dayOfTheWeek + leapMod[month]) % 7;
@@ -52,13 +51,13 @@ int main()
 
         month = 0;      // Reset to January
         isLeap = false; // Reset isLeap
-        currentYear++; // Update CurrentYear
+        currentYear++;  // Update CurrentYear
     }
-    
-    outputFile << to_string(dayTrack[5]); //sat
+
+    outputFile << to_string(dayTrack[5]);       //sat
     outputFile << " " + to_string(dayTrack[6]); // sun
     outputFile << " " + to_string(dayTrack[0]); // mon
-    outputFile << " " + to_string(dayTrack[1]); 
+    outputFile << " " + to_string(dayTrack[1]);
     outputFile << " " + to_string(dayTrack[2]);
     outputFile << " " + to_string(dayTrack[3]);
     outputFile << " " + to_string(dayTrack[4]) << endl;
