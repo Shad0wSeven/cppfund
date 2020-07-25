93ec106afd2b27b6d79960ebcb6d032f2b0d2fbf
Shad0w7 <ayush.nayak@gmail.com>
2020-07-24 17:39:58 -0700

    fixed uncommited stuff

--- USACO 3.1/Contact/contact.cpp
+++ USACO 3.1/Contact/contact.cpp
@@ -1,6 +1,6 @@
 /*
 ID: ayushn.2
-TASK: XXXXXXXXX
+TASK: contact
 LANG: C++11
 */
 #include <iostream>
@@ -18,8 +18,8 @@ struct num {
 
 int main()
 {
-    ofstream fout ("X.out");
-    ifstream fin ("X.in");
+    ofstream fout ("contact.out");
+    ifstream fin ("contact.in");
     int A, B, N;
     fin >> A >> B >> N;
     string s = "";
@@ -28,9 +28,33 @@ int main()
         fin >> x;
         s += x;
     }
+    vector<string> strings;
     // Method -> just add everything to a list, order that list and BOOM done.
     cout << s.length() << endl;
-    for(int i = 0; i < s.length(); i++)
+    for(int i = 0; i < s.length(); i++) {
+        for(int j = 0; j < B; j++){
+            string construct = "";
+            for(int x = 0; x < j; x++) {
+                construct += s.at(i + x);
+            }
+            strings.push_back(construct);
+        }
+    }
+
+    sort(strings.begin(), strings.end());
+    int cnt = 0;
+    int crrCnt = 0;
+    string curritem = "";
+    while(cnt > strings.size()) {
+        if(strings[cnt] != curritem) {
+            fout << curritem << endl;
+            cout << crrCnt << endl;
+            crrCnt = 0;
+            curritem = strings[cnt];
+        }
+        cnt++;
+
+    }
     
     fout << "NotSetYet" << endl;
     return 0;
---
+++ USACO 3.1/Stamps/stamps.cpp
@@ -0,0 +1,22 @@
+/*
+ID: ayushn.2
+TASK: stamps
+LANG: C++11
+*/
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ofstream fout ("stamps.out");
+    ifstream fin ("stamps.in");
+    int N, K;
+    fin >> K >> N;
+    
+    fout << "NotSetYet" << endl;
+    return 0;
+    
+}
