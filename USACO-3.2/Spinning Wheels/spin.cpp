d9d01eacc90d2064fd3a0147e7db00c89b11a0a7
Shad0w7 <ayush.nayak@gmail.com>
2020-08-03 17:36:32 -0700

    spin

--- USACO-3.2/Spinning Wheels/spin.cpp
+++ USACO-3.2/Spinning Wheels/spin.cpp
@@ -20,6 +20,16 @@ struct wheel {
     vector<wedge> wedges;
 };
 
+bool checkGap(vector<wheel> toCheck) {
+    for(int i = 0; i < 360; i++) {
+        for(int j = 0; j < 5; j++) {
+            for(int k = 0; k < toCheck[j].wedgeNum; k++) {
+                
+            }
+        }
+    }
+}
+
 int main()
 {
     ofstream fout ("spin.out");
@@ -37,7 +47,53 @@ int main()
     }
     // Everything gathered
 
-    int a1[359]
+    vector<wheel> current = smh;
+    int cnt = 0;
+    while (true) {
+        // First Check 
+        if(checkGap(current)) {
+            cout << cnt << endl;
+            return 0;
+        }
+        // Then Update
+        for(int i = 0; i < 5; i++) {
+            wheel x = current[i];
+            for(int j = 0; j < x.wedgeNum; j++) {
+                int a = x.wedges[j].start + x.speed;
+                int b = x.wedges[j].end + x.speed;
+                x.wedges[j].start = a % 360;
+                x.wedges[j].end = b % 360;
+            }
+            current[i] = x;
+        }
+        cnt ++;
+        // Check if Original
+        bool original = false;
+        for(int i = 0; i < 5; i++) {
+
+            for(int j = 0; j < current[i].wedgeNum; j++) {
+
+                if(current[i].wedges[j].start != smh[i].wedges[j].start) {
+                    original = true;
+                    break;
+
+                } 
+
+                if(current[i].wedges[j].start != smh[i].wedges[j].start) {
+                    original = true;
+                    break;
+                }
+            }
+            if(original == true)
+                break;
+        }
+        if(original == false) {
+            cout << "none" << endl;
+            cout << cnt << endl;
+            return 0;
+        }
+
+    }
 
     fout << "NotSetYet" << endl;
     return 0;
