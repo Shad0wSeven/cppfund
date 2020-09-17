ee7660665c690c1611360816dd4e524d89cc4ef7
Shad0w7 <ayush.nayak@gmail.com>
2020-09-17 14:19:34 -0700

    lineup finished, should move

---
+++ Codeforces/firsttask.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int t, maze[100][100], mazeAns[100];
+    cin >> t;
+    for(int i = 0; i < t; i++) {
+        int x;
+        cin >> x;
+        for(int j = 0; j < x; j++) {
+            cin >> maze[i][j];
+        }
+// Sor the array and do reverse greedy
+    }
+    return 0;
+}
