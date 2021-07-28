76972d4d3558a2b69fab7d8217ee0d0bdf10eb46
Shad0w7 <ayush.nayak@gmail.com>
2021-07-28 12:40:10 -0700

    hfs

---
+++ USACO Gold/Jan 2017/hfs.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<pair<int, int> > vpi;
+
+#define FAR(i, a, b) for (int i=a; i<b; i++)
+#define FOR(i, a) for (int i=0; i<a; i++)
+
+#define fi first
+#define se second
+#define endl "\n"
+
+int N, K;
+
+int main () {
+	ifstream cin("hps.in");
+	cin >> N >> K;
+
+	return 0;
+}
+
+
